b2904e6035542499c0140b1bab5b5f88272f5d08
anoopiitr <anoop.iitr@rocketmail.com>
2013-10-29 16:04:46 +0530

    Max share price finder module

---
+++ MaxSharePrice/src/CustomExceptions.py
@@ -0,0 +1,9 @@
+
+class BadInputException(Exception): 
+    """
+    Raised when number of companies mismatch with number of proceeding records
+    """
+    pass
+    
+class PriceDataAbsentException(Exception): pass
+    
